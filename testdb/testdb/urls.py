a47bdb0e41741027623ccdc86debb0c75337248d
edison <2499090390@qq.com>
2022-04-08 11:12:24 +0800

    add index

--- testdb/testdb/urls.py
+++ testdb/testdb/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from firstapp import views
 urlpatterns = [
+    path('', views.index),
+
+
+
+
+
+
     path('firstapp/', include('firstapp.urls')),
     path('admin/', admin.site.urls),
 ]
